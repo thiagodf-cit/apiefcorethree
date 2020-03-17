567f6b4a5e4b1191080f00302efdfc1c0eb9ef8c
thiagodf <thiagodf@ciandt.com>
2020-03-17 15:57:09 -0300

    fix model product

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,12 +12,16 @@ namespace EfCoreThree.Models
         [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
         public string Title { get; set; }
         
-        [MinLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres.")]
+        [MaxLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres.")]
         public string Description { get; set; }
         
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
         public decimal Price { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+        public int CategoryId { get; set; }
         
         public Category Category { get; set; }
     }
