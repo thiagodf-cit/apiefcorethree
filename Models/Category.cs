8ddc9ddcb0410330acd3b65fb563b87bd56e0b54
thiagodf <thiagodf@ciandt.com>
2020-03-17 11:10:44 -0300

    create models category

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreThree.Models
+{
+    public class Category
+    {
+        
+        [Key]
+        
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        
+        public string Titulo { get; set; }
+        
+    }
+}
