9718e877a20319b308bdf01e8e71ed0cbaf4d85b
thiagodf <thiagodf@ciandt.com>
2020-03-17 12:02:44 -0300

    create model product

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreThree.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        public string Title { get; set; }
+        
+        [MinLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres.")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+        public decimal Price { get; set; }
+        
+        public Category Category { get; set; }
+    }
+}
